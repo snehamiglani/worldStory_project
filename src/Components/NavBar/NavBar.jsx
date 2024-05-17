4d39c1c764e217de27f0ce38ee371ff77513364d
nickpratyaksh <nickpratyaksh@gmail.com>
2024-05-17 23:49:39 +0530

    phone-nav

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,44 +1,67 @@
-import React from "react";
+import { React, useState } from "react";
 import styles from "./NavBar.module.css";
 
 export default function NavBar() {
+  const [showPhoneNav, setShowPhoneNav] = useState(false);
+
   return (
-    <div className={styles.container}>
-      <div style={{ display: "flex" }}>
-        <div className={styles.hamburger}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className={styles.icon}
+    <div>
+      {showPhoneNav ? <PhoneNav /> : <></>}
+      <div className={styles.container}>
+        <div style={{ display: "flex" }}>
+          <div
+            className={styles.hamburger}
+            onClick={() => {
+              setShowPhoneNav(!showPhoneNav);
+            }}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
-            />
-          </svg>
-        </div>
-        <div className={styles.logo_container}>
-          <div className={styles.logo} style={{ color: "#d00494" }}>
-            WORLD
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className={styles.icon}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+              />
+            </svg>
           </div>
-          <div className={styles.logo}>STORY</div>
+          <div className={styles.logo_container}>
+            <div className={styles.logo} style={{ color: "#d00494" }}>
+              WORLD
+            </div>
+            <div className={styles.logo}>STORY</div>
+          </div>
         </div>
+        <ul className={styles.list_container}>
+          <li className={styles.list_item}>NEWS</li>
+          <li className={styles.list_item}>FASHION</li>
+          <li className={styles.list_item}>LIFESTYLE</li>
+          <li className={styles.list_item}>TRAVEL</li>
+        </ul>
+        <img
+          src="/social_media_icons.png"
+          alt=""
+          className={styles.social_media_icons}
+        />
       </div>
-      <ul className={styles.list_container}>
-        <li className={styles.list_item}>NEWS</li>
-        <li className={styles.list_item}>FASHION</li>
-        <li className={styles.list_item}>LIFESTYLE</li>
-        <li className={styles.list_item}>TRAVEL</li>
+    </div>
+  );
+}
+
+function PhoneNav() {
+  return (
+    <div className={styles.phone_nav}>
+      <ul className={styles.phone_list_container}>
+        <li className={styles.phone_list_item}>NEWS</li>
+        <li className={styles.phone_list_item}>FASHION</li>
+        <li className={styles.phone_list_item}>LIFESTYLE</li>
+        <li className={styles.phone_list_item}>TRAVEL</li>
       </ul>
-      <img
-        src="/social_media_icons.png"
-        alt=""
-        className={styles.social_media_icons}
-      />
     </div>
   );
 }
