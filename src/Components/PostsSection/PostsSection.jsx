4c804b69790675d5466ec12939628d8dbb0d4985
sneha <miglanisneha7@gmail.com>
2024-05-21 15:46:08 +0530

    first commit

--- src/Components/PostsSection/PostsSection.jsx
+++ src/Components/PostsSection/PostsSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./PostsSection.module.css";
 
 const posts = [
@@ -19,8 +19,6 @@ const interestingPosts = [
 ];
 
 export default function PostsSection() {
-  const [headerText, setHeaderText] = useState("INTERESTING");
-
   return (
     <div className={styles.container}>
       <div className={styles.latest_post_container}>
@@ -32,7 +30,7 @@ export default function PostsSection() {
       </div>
       <div className={styles.secondary_section}>
         <div className={styles.secondary_primary_container}>
-          <div className={styles.header}>{headerText}</div>
+          <div className={styles.header}>INTERESTING</div>
           <div className={styles.bar}></div>
           <div>
             {interestingPosts.map((post, i) => {
@@ -41,7 +39,7 @@ export default function PostsSection() {
           </div>
         </div>
         <div>
-          <div className={styles.header}>{headerText}</div>
+          <div className={styles.header}>INTERESTING</div>
           <div className={styles.bar}></div>
           <div className={styles.secondary_secondary_container}>
             <img src="/rectangle_16.jpg" alt="" className={styles.image} />
